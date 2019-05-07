d233b01148383ccdb481551babcd5524114eb513
Skullbonk <reagan.fairbanks@gmail.com>
2019-05-07 13:54:31 -0600

    redid scenes, it's a lot nicer now

--- src/sort/view/SortStage.java
+++ src/sort/view/SortStage.java
@@ -1,10 +1,13 @@
 package sort.view;
 
 import javafx.application.*;
+import javafx.collections.FXCollections;
 import javafx.event.*;
 import javafx.scene.*;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -13,72 +16,76 @@ import javafx.stage.*;
 
 public class SortStage extends Application implements EventHandler<ActionEvent>
 {
+	Stage window;
 	FileChooser files = new FileChooser();
 	
-	Button saveButton;
-	Button loadButton;
-	Button sortButton;
+	VBox layout1;
+	VBox layout2;
 	
-	TextField entryField;
-	ChoiceBox<String> typeBox;
-	ChoiceBox<String> sortByBox;
-	ChoiceBox<String> algoBox;
+	Scene scene1;
+	Scene scene2;
+	
+	Canvas sortCanvas;
+	
 	
 	public SortStage()
 	{
 		super();
+		layout1 = new VBox();
+		layout2 = new VBox();
+		scene1 = new Scene(layout1, 1244, 720);
+		scene2 = new Scene(layout2, 1244, 720);
 	}
+	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		setupStage(primaryStage);
+		window = primaryStage;
+		setupStage(window);
 	}
 	
-	public void setupStage(Stage primaryStage)
+	
+	public void setupStage(Stage window)
 	{
-//		buttons
-		saveButton = new Button();
-		loadButton = new Button();
-		sortButton = new Button();
-		
-		saveButton.setText("Save");
-		loadButton.setText("Load");
-		sortButton.setText("Sort");
-		
-		saveButton.setOnAction(e -> files.showSaveDialog(primaryStage));
-		loadButton.setOnAction(e -> files.showOpenDialog(primaryStage));
-		sortButton.setOnAction(e -> sort(primaryStage, getChoice(algoBox)));
-		
-//		data entry
-		typeBox = new ChoiceBox<String>(); // where you select the type of data
-		sortByBox = new ChoiceBox<String>(); // where you select how you want it sorted
-		entryField = new TextField(); // where you enter the data
-		algoBox = new ChoiceBox<String>(); // where you choose the sorting algorithm to use
-		
-		typeBox.getItems().addAll("Integer", "String", "Double");
-		typeBox.getSelectionModel().select(0);
-		
-		sortByBox.getItems().addAll(getSortBy(getChoice(typeBox)));
-		
-		algoBox.getItems().addAll("Quicksort", "Merge sort", "Insertion sort", "Heapsort");
-		algoBox.getSelectionModel().select(0);
-		
-//		utility stuff
-		VBox layout = new VBox();
-		layout.getChildren().add(saveButton);
-		layout.getChildren().add(loadButton);
-		layout.getChildren().add(typeBox);
-		layout.getChildren().add(entryField);
-		layout.getChildren().add(algoBox);
-		layout.getChildren().add(sortButton);
-		
-		Scene scene1 = new Scene(layout, 1244, 720);
-		primaryStage.setScene(scene1); // assigns this scene to the stage (window)
-		primaryStage.show(); // displays to user	
-		
-
+		setupScene1(layout1);
+		setupScene2(layout2);
+		window.setScene(scene1);
+		window.setTitle("Setup");
+		window.show();
 	}
+	
+	
+	public void setupScene1(VBox layout1) // for setting up sort
+	{
+		Button sortSceneButton = new Button("To sort page");
+		sortSceneButton.setOnAction(e -> {window.setScene(scene2); window.setTitle("Sort");});
+		
+		ChoiceBox<String> dataTypeBox = new ChoiceBox<String>();
+		dataTypeBox.setItems(FXCollections.observableArrayList(
+				"String", "Integer", "Double"));
+		
+		Label enterDataLabel = new Label("Enter values here, separated by commas");
+		TextField entryField = new TextField();
+		
+		layout1.getChildren().addAll(sortSceneButton, enterDataLabel, entryField);
+		
+	}
+	
+	
+	public void setupScene2(VBox layout2)
+	{
+		Button setupSceneButton = new Button("To setup page");
+		setupSceneButton.setOnAction(e -> {window.setScene(scene1); window.setTitle("Setup");});
+		
+		sortCanvas = new Canvas(800, 600);		
+		layout2.getChildren().addAll(setupSceneButton, sortCanvas);
+		
+		
+	}
+	
+	
+	
 	
 	
 	@Override
@@ -95,48 +102,7 @@ public class SortStage extends Application implements EventHandler<ActionEvent>
 	}
 	
 	
-	private String[] getSortBy(String type)
-	{
-		String[] stringSorts = {"Alphabetical", "Length"};
-		String[] intSorts = {"Greatest -> Least", "Least -> Greatest"};
-		String[] doubleSorts = {"Greatest -> Least", "Least -> Greatest"};
-		String[] sortBy = {""};
 
-		switch(type)
-		{
-		case "String":
-			sortBy = stringSorts;
-			break;
-		case "Integer":
-			sortBy = intSorts;
-			break;
-		case "Double":
-			sortBy = doubleSorts;
-			break;
-		}
-		return sortBy;
-	}
-	
-	
-	private void sort(Stage primaryStage, String algorithm)
-	{
-		VBox layout = new VBox();
-		
-		Scene scene2 = new Scene(layout, 1244, 720);
-		primaryStage.setScene(scene2);
-	}
-	
-	private void load()
-	{
-		
-	}
-	
-	
-	private void save()
-	{
-		
-	}
-	
 	public static void main(String[] args)
 	{
 		launch(args);
