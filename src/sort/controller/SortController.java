03a1c5cbcd91fbfb61a0b587a42f5a8d6e3aa7a2
Skullbonk <reagan.fairbanks@gmail.com>
2019-05-13 13:13:36 -0600

    fixed text file assertion

--- src/sort/controller/SortController.java
+++ src/sort/controller/SortController.java
@@ -3,7 +3,10 @@ package sort.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 import javafx.application.*;
@@ -11,9 +14,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
@@ -26,15 +31,17 @@ import sort.model.*;
 public class SortController extends Application
 {
 	private Stage stage;
+	private Scene scene;
 	private TabPane tabs;
 	private Sorter sorter;
+//	private SingleSelectionModel<Tab> selectionModel;
 	@FXML private Tab setupTab;
 	@FXML private Tab sortTab;
 	@FXML private Label enterDataLabel;
 	@FXML private TextField entryField;
 	@FXML private Button sortButton;
 	@FXML private Button loadButton;
-	@FXML private ChoiceBox algoBox;
+	@FXML private ChoiceBox<String> algoBox;
 	
 	public SortController()
 	{
@@ -53,7 +60,9 @@ public class SortController extends Application
 		tabs = (TabPane) fxml.load(fxmlStream);
 		stage = primaryStage;
 		stage.setTitle("SortBoy");
-		stage.setScene(new Scene(tabs, 800, 600));
+		scene = new Scene(tabs, 800, 600);
+		stage.setScene(scene);
+		tabs.getSelectionModel().select(setupTab);
 		stage.show();
 	}
 	
@@ -71,8 +80,26 @@ public class SortController extends Application
 			loadData();
 		});
 		
+		sortButton.setOnAction((event) -> {
+			submitSort();
+			// change to sort tab
+		});
+		
 	}
 	
+	
+	private void submitSort()
+	{
+		if(sorter == null)
+		{
+			sorter = new Sorter(entryField.getText());
+		}
+		else
+		{
+			sorter.injectData(entryField.getText());
+			System.out.println(entryField.getText());
+		}
+	}
 	
 	
 	private void loadData()
@@ -82,17 +109,22 @@ public class SortController extends Application
 			String text = "";
 			FileChooser fileChooser = new FileChooser();
 			File textFile = fileChooser.showOpenDialog(stage);
-			String extension = textFile.toString();
+			String extension = textFile.getPath();
 			extension = extension.substring(extension.lastIndexOf("."));
-			Scanner fileScanner = new Scanner(textFile);
-			assert extension.equals("txt");
-			text = fileScanner.toString();
+			System.out.println("File extension: " + extension);
+			assert extension.equalsIgnoreCase(".txt");
+//			FileReader reader = new FileReader(textFile.getPath());
+			text = new String(Files.readAllBytes(Paths.get(textFile.getPath())));
 			entryField.setText(text);
 		}
 		
 		catch(FileNotFoundException exception)
 		{
 			entryField.setText("error loading file: file not found");
+		}
+		catch(IOException ioError)
+		{
+			entryField.setText("error loading file");
 		}
 		catch(AssertionError assertionError)
 		{
--- src/sort/model/Sorter.java
+++ src/sort/model/Sorter.java
@@ -1,14 +1,23 @@
 package sort.model;
 
-public class Sorter
+import javafx.scene.canvas.Canvas;
+
+public class Sorter extends Canvas
 {
 
 	public Sorter()
 	{
+		super();
 		// TODO Auto-generated constructor stub
 	}
 	
 	public Sorter(String toSort)
+	{
+		super();
+		injectData(toSort);
+	}
+	
+	public void injectData(String toSort)
 	{
 		
 	}
